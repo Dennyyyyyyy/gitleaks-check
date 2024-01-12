8572dfb4d95b6122360b6186b273a6367a188133
Dennyyyyyyy <den.grinyko@gmail.com>
2024-01-12 20:54:33 +0200

    version 1.0.0

--- pre_commit_check.sh
+++ pre_commit_check.sh
@@ -1,89 +1,86 @@
 #!/bin/bash
 
-# Визначення операційної системи
+# Define your OS
 os_name=$(uname)
 
-# Функція для виведення повідомлення про помилку та вихід зі скрипту
+# Function to display an error message and exit the script
 error_exit() {
   echo "$1" >&2
   exit 1
 }
 
-# Функція для встановлення pre-commit
+# Check if Python is installed
+check_python() {
+  if command -v python3 &>/dev/null; then
+    echo "Python is installed"
+  else
+    error_exit "Didn't find Python in your system. Please install Python first before using this script."
+  fi
+}
+
+# Function installation pre-commit to your system
 install_precommit() {
-  echo "Встановлення pre-commit..."
+  echo "Installation pre-commit..."
   if command -v pip3 &>/dev/null; then
     pip3 install pre-commit
   elif command -v pip &>/dev/null; then
     pip install pre-commit
   else
-    error_exit "pip не знайдено. Встановіть pip перед встановленням pre-commit."
+    error_exit "Didn't find pip in your OS. Please install pip first before installation pre-commit"
   fi
 }
 
-# Функція для створення та наповнення .pre-commit-config.yaml
-create_precommit_config() {
-  echo "Створення .pre-commit-config.yaml..."
-  echo "- repo: https://github.com/gitleaks/gitleaks" > .pre-commit-config.yaml
-  echo "  rev: v8.18.1" >> .pre-commit-config.yaml
-  echo "  hooks:" >> .pre-commit-config.yaml
-  echo "    - id: gitleaks" >> .pre-commit-config.yaml
-}
-
-# Функція для виконання команд pre-commit
-run_precommit_commands() {
-  git config pre-commit.gitleaks true
-  echo "Виконання команд pre-commit..."
-  pre-commit autoupdate
-  #pre-commit install
-  pre-commit install -f --hook-type pre-commit
-  pre-commit
-}
-
-# Визначення наявності Python
-check_python() {
-  if command -v python3 &>/dev/null; then
-    echo "Python встановлено"
-  else
-    error_exit "Python не знайдено. Встановіть Python перед використанням цього скрипта."
-  fi
-}
-
-# Визначення наявності pre-commit
+# Define and install pre-commit
 check_precommit() {
   if command -v pre-commit &>/dev/null; then
-     echo "Команда pre-commit встановлена."
-else
-    echo "Помилка: команда pre-commit не встановлена."
-    install_precommit
-    echo "pre-commit успішно встановлено"
-    # Додайте код чи повідомлення про дії у разі відсутності команди.
-fi
-}
-
-# Визначення наявності .pre-commit-config.yaml
-check_precommit_config() {
-  if [ -e .pre-commit-config.yaml ]; then
-    echo ".pre-commit-config.yaml вже існує"
+     echo "The pre-commit command is already installed."
   else
+    echo "The pre-commit command is missing in your system"
+    install_precommit 
+    echo "pre-commit has been installed succesfully"
+    echo "Executing commands pre-commit..."
+    pre-commit autoupdate
+    pre-commit install -f --hook-type pre-commit
+    pre-commit
+    git config pre-commit.gitleaks true
     create_precommit_config
-    echo ".pre-commit-config.yaml успішно створено"
   fi
 }
 
-# Виведення інформації про операційну систему
-echo "Операційна система: $os_name"
+# Function of creating .pre-commit-config.yaml file
+create_precommit_config() {
+  if [ -e .pre-commit-config.yaml ]; then
+    echo ".pre-commit-config.yaml is already exist. Make a backup of previous file"
+    backup_pre_commit_yaml=".pre-commit-config-$(date +"%Y%m%d%H%M%S").yaml"
+    mv .pre-commit-config.yaml "$backup_pre_commit_yaml"
+    echo "Backup has been created: $backup_pre_commit_yaml"
+    echo "Створення .pre-commit-config.yaml..."
+    echo "- repo: https://github.com/gitleaks/gitleaks" > .pre-commit-config.yaml
+    echo "  rev: v8.18.1" >> .pre-commit-config.yaml
+    echo "  hooks:" >> .pre-commit-config.yaml
+    echo "    - id: gitleaks" >> .pre-commit-config.yaml
+    echo ".pre-commit-config.yaml is created"
+  fi
+}
 
-# Перевірка наявності Python
-check_python
-
-# Перевірка наявності pre-commit
-check_precommit
-
-# Перевірка наявності .pre-commit-config.yaml
-check_precommit_config
-
-# Виконання команд pre-commit
-run_precommit_commands
+for arg in "$@"; do
+  echo "Your operation system is: $os_name"
+  echo "Argument handling: $arg"
+    case "$arg" in
+    check_python)
+      check_python
+      ;;
+    install_precommit)
+      check_precommit
+      ;;
+    create_config)
+      check_precommit_config
+      ;;
+    *)
+      echo "Sorry, don't know that argument: $1"
+      exit 1
+      ;;
+  esac
+done
 
 echo "Great news! Now your commits are protected from leaks"
